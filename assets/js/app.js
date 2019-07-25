50a9927263d6ebf689a446662d3888b46fe8a8ef
akaryrye <ralldrin@gmail.com>
2019-07-24 23:36:12 -0700

    added form inputs, and a test output

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
-$('.click').on('click', function() {
-    alert('test');
-    $(this).text("this is a jquery test"); 
+
+
+$('#submit').on('click', function(e) {
+    e.preventDefault();
+    $("#name").text($("#train-name").val());
+    $("#dest").text($("#destination").val());
+    $("#nextArr").text($("#next-arrival").val()); 
+    $("#interval").text($("#time-between").val()); 
 });

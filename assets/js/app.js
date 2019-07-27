a1384b1c3ef94fcfa8afa92bf6284d5d43deee1a
akaryrye <ralldrin@gmail.com>
2019-07-27 01:20:45 -0700

    db connected. list populates on load and updates on submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,57 @@
+var config = {
+    apiKey: "AIzaSyANasD6oaAkIJSHg5qPW39CsI9FViLHsj4",
+    authDomain: "ralldrin-test-fec5f.firebaseapp.com",
+    databaseURL: "https://ralldrin-test-fec5f.firebaseio.com",
+    projectId: "ralldrin-test-fec5f",
+    storageBucket: "ralldrin-test-fec5f.appspot.com",
+    messagingSenderId: "864145895805",
+    appId: "1:864145895805:web:ddc9704a2426812b"
+};
+
+firebase.initializeApp(config);
+
+var DB = firebase.database();
 
 
-$('#submit').on('click', function(e) {
+let trains = {};
+let index;
+let snapshot;
+
+DB.ref('TrainApp/trains').once('value', function (snapshot) {
+    trains = snapshot.val();
+    index = Object.keys(trains).length;
+    populateList();
+});
+
+console.log(index)
+
+function addTrain() {
+    let obj = {};
+    obj.name = $("#train-name").val();
+    obj.dest = $("#destination").val();
+    obj.nextArr = $("#next-arrival").val();
+    obj.interval = $("#time-between").val();
+    trains[index] = obj;
+    index++;
+    DB.ref('TrainApp/trains').set(trains)
+}
+
+function populateList() {
+    $("#output").empty();
+    for (let i = 0; i < index; i++) {
+
+        $("#output").prepend(
+            `<h4>Name: ${trains[i].name}, 
+            Destination: ${trains[i].dest}
+            Next Arrival: ${trains[i].nextArr}
+            Interval: ${trains[i].interval}</h4>`);
+    }
+}
+
+$('#submit').on('click', function (e) {
     e.preventDefault();
-    $("#name").text($("#train-name").val());
-    $("#dest").text($("#destination").val());
-    $("#nextArr").text($("#next-arrival").val()); 
-    $("#interval").text($("#time-between").val()); 
+    addTrain();
+    populateList();
+
+
 });

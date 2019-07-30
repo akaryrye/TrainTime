eeb7b3a505e77146c9f86928496adcddce298a08
akaryrye <ralldrin@gmail.com>
2019-07-30 00:22:35 -0700

    styles, form validation, live time display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,65 @@
-var config = {
-    apiKey: "AIzaSyANasD6oaAkIJSHg5qPW39CsI9FViLHsj4",
-    authDomain: "ralldrin-test-fec5f.firebaseapp.com",
-    databaseURL: "https://ralldrin-test-fec5f.firebaseio.com",
-    projectId: "ralldrin-test-fec5f",
-    storageBucket: "ralldrin-test-fec5f.appspot.com",
-    messagingSenderId: "864145895805",
-    appId: "1:864145895805:web:ddc9704a2426812b"
-};
-
-firebase.initializeApp(config);
-
-var DB = firebase.database();
 
 
-let trains = {};
-let index;
-let snapshot;
+$(document).ready(function () {
+    
+    // firebase config / variables / etc
+    var config = {
+        apiKey: "AIzaSyANasD6oaAkIJSHg5qPW39CsI9FViLHsj4",
+        authDomain: "ralldrin-test-fec5f.firebaseapp.com",
+        databaseURL: "https://ralldrin-test-fec5f.firebaseio.com",
+        projectId: "ralldrin-test-fec5f",
+        storageBucket: "ralldrin-test-fec5f.appspot.com",
+        messagingSenderId: "864145895805",
+        appId: "1:864145895805:web:ddc9704a2426812b"
+    };
+    firebase.initializeApp(config);
+    var DB = firebase.database();
 
-DB.ref('TrainApp/trains').once('value', function (snapshot) {
-    trains = snapshot.val();
-    index = Object.keys(trains).length;
-    populateList();
-});
-
-console.log(index)
-
-function addTrain() {
-    let obj = {};
-    obj.name = $("#train-name").val();
-    obj.dest = $("#destination").val();
-    obj.nextArr = $("#next-arrival").val();
-    obj.interval = $("#time-between").val();
-    trains[index] = obj;
-    index++;
-    DB.ref('TrainApp/trains').set(trains)
-}
-
-function populateList() {
-    $("#output").empty();
-    for (let i = 0; i < index; i++) {
-
-        $("#output").prepend(
-            `<h4>Name: ${trains[i].name}, 
-            Destination: ${trains[i].dest}
-            Next Arrival: ${trains[i].nextArr}
-            Interval: ${trains[i].interval}</h4>`);
+    let trains = {};
+    let index;
+    let snapshot;
+    
+    function currentTime () {
+        let time = moment().format("hh:mm:ssA");
+        $("#curr-time").html(`<h1>${time}</h1>`)
     }
-}
 
-$('#submit').on('click', function (e) {
-    e.preventDefault();
-    addTrain();
-    populateList();
+    // add a child train to trains object on database
+    function addTrain() {
+        let obj = {};
+        obj.name = $("#train-name").val();
+        obj.dest = $("#destination").val();
+        obj.nextArr = $("#next-arrival").val();
+        obj.interval = $("#time-between").val();
+        trains[index] = obj;
+        index++;
+        DB.ref('TrainApp/trains').set(trains)
+    }
 
+    // Loop through trains object and display
+    function populateList() {
+        $("#output").empty();
+        for (let i = 0; i < index; i++) {
+            $("#output").prepend(
+                `<tr><th scope="row">${trains[i].name}</th>, 
+                <td>${trains[i].dest}</td>
+                <td>${trains[i].nextArr}</td>
+                <td>${trains[i].interval}</td>`);
+        }
+    }
 
+    let clock = setInterval(currentTime, 1000);
+
+    // load stored list on page load
+    DB.ref('TrainApp/trains').once('value', function (snapshot) {
+        trains = snapshot.val();
+        index = Object.keys(trains).length;
+        populateList();
+    });
+
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        addTrain();
+        populateList();
+    });
 });
